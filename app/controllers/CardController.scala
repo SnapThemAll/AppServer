2fd37566b7fba25c8b1654d6a64934ab067a7596
Grégoire Clément <gregunz.io@gmail.com>
2017-04-09 19:09:02 +0200

    commit before leaving to lausanne

--- app/controllers/CardController.scala
+++ app/controllers/CardController.scala
@@ -1,18 +1,15 @@
 package controllers
 
-import java.io.File
-import java.util.UUID
 import javax.inject.Inject
 
 import com.mohiva.play.silhouette.api.Silhouette
 import models.services.CardService
+import play.api.Configuration
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json.Json
-import play.api.mvc.{BodyParsers, Controller}
+import play.api.mvc.Controller
 import utils.auth.DefaultEnv
 
 import scala.concurrent.Future
-import scala.util.Random
 
 /**
   * Class that handles the request related to a track
@@ -20,19 +17,38 @@ import scala.util.Random
   *
   * Check conf.routes to see what URL to use for your requests
   */
-class CardController @Inject()(cardService: CardService, silhouette: Silhouette[DefaultEnv]) extends Controller {
+class CardController @Inject()(configuration: Configuration ,cardService: CardService, silhouette: Silhouette[DefaultEnv]) extends Controller {
 
   import Utils._
 
-  def uploadPicture(cardName: String) = silhouette.UserAwareAction.async(parse.temporaryFile) { implicit request =>
+  def uploadPicture(cardName: String) = silhouette.UserAwareAction.async(parse.multipartFormData) { implicit request =>
     verifyAuthentication(request) { identity =>
+      Future.successful(request.body.file("picture").map { picture =>
+        import java.io.File
+        val filename = picture.filename
+        val contentType = picture.contentType
+        val providerKey = identity.loginInfo.providerKey
+        val absPathToSave = configuration.getString("my.data.path").getOrElse("/tmp/")
+        val pictureFolderURI = absPathToSave + s"$providerKey/$cardName/"
+        val pictureURI = pictureFolderURI + filename
+
+        new File(pictureFolderURI).mkdirs()
+        picture.ref.moveTo(new File(pictureURI))
+        Ok("File uploaded")
+      }.getOrElse {
+        UnprocessableEntity("Could not upload file")
+      }
+      )
+      /*
       val userID = identity.userID
-      val pictureURI = s"/tmp/picture/$userID/$cardName/"
+      val pictureURI = s"/$cardName.jpg/"
       request.body.moveTo(new File(pictureURI))
       cardService.savePicture(userID, cardName, pictureURI).map( success => {
         Ok("File uploaded, your score is: " + Random.nextDouble()*10)
       })
+      */
     }.recover(recoverFromInternalServerError)
   }
+
 
 }

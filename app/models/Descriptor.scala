662f64fdb7cba47579c34a1669ea9171850782b9
Grégoire Clément <gregunz.io@gmail.com>
2017-05-21 22:46:03 +0200

    update

--- app/models/Descriptor.scala
+++ app/models/Descriptor.scala
@@ -8,7 +8,7 @@ import org.bytedeco.javacpp.opencv_core.Mat
 /**
   * Created by Greg on 21.05.2017.
   */
-case class Descriptor(descriptorMatrix: Mat) {
+case class Descriptor(fileName: String, descriptorMatrix: Mat) {
 
   def similarityWith(that: Descriptor): Float =
     similarity(this.descriptorMatrix, that.descriptorMatrix)
@@ -16,7 +16,7 @@ case class Descriptor(descriptorMatrix: Mat) {
 
 object Descriptor {
 
-  def fromImageFile(imageFile: File): Descriptor = Descriptor(computeDescriptor(imageFile))
+  def fromImageFile(imageFile: File): Descriptor = Descriptor(imageFile.getName, computeDescriptor(imageFile))
 
   def fromImagePath(imagePath: String): Descriptor = fromImageFile(new File(imagePath))
 

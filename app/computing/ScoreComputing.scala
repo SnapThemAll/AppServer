dc502bcd0b137e9b2b8afa520528e2d5deafe4c2
gregunz <mail@gregunz.io>
2017-05-27 19:31:25 +0200

    v1.0.1

--- app/computing/ScoreComputing.scala
+++ app/computing/ScoreComputing.scala
@@ -3,6 +3,8 @@ package computing
 import models.{Descriptor, ValidationCategory}
 import utils.Logger
 
+import scala.util.Random
+
 object ScoreComputing extends Logger {
 
   def computeScore(newDescriptor: Descriptor, validationCategory: ValidationCategory) : (Float, ValidationCategory) = {
@@ -10,11 +12,29 @@ object ScoreComputing extends Logger {
     val updatedValidationCategory = validationCategory.computeSimilarites(newDescriptor)
     val marginalGain = updatedValidationCategory.marginalGain(validationCategory)
 
-    val score = marginalGain / updatedValidationCategory.averageGain // SCORE = GAIN / AVERAGE GAIN
+    var score: Float = marginalGain / updatedValidationCategory.averageGain // SCORE = GAIN / AVERAGE GAIN
 
     log(s"Category ${validationCategory.category} improved by $marginalGain} (the bigger the better). Score: $score")
 
-    Math.min(score * 5, 10f) -> updatedValidationCategory
+
+    score = Math.min(score * 5, 10f) // [0, 10]
+
+    if(score > 0 && score < 1){
+      score = 1f + randomScore(1f, .5f) // a little help if score is between [0, 1[
+    }
+    if(score == 0){
+      score += randomScore(.2f, 1f) // a little help (sometimes) if score == 0
+    }
+
+    score -> updatedValidationCategory
+  }
+
+  def randomScore(percentage: Float, to: Float): Float = {
+    if(Random.nextFloat() < percentage) {
+      Random.nextFloat() * to
+    } else {
+      0f
+    }
   }
 
 }

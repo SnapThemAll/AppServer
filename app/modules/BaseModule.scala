b7427215f5e0cc69e760418f6d0048cd2c4fd5f5
gregunz <mail@gregunz.io>
2017-05-27 14:57:22 +0200

    Message added

--- app/modules/BaseModule.scala
+++ app/modules/BaseModule.scala
@@ -22,11 +22,13 @@ class BaseModule extends AbstractModule with ScalaModule with AkkaGuiceSupport {
     bind[ValidationCategoryDAO].to[ValidationCategoryDAOMongo]
     bind[FeedbackDAO].to[FeedbackDAOMongo]
     bind[UpdateDAO].to[UpdateDAOMongo]
+    bind[MessageDAO].to[MessageDAOMongo]
 
     bind[AuthTokenService].to[AuthTokenServiceImpl]
     bind[UserService].to[UserServiceImpl]
     bind[CardService].to[CardServiceImpl]
     bind[FeedbackService].to[FeedbackServiceImpl]
     bind[UpdateService].to[UpdateServiceImpl]
+    bind[MessageService].to[MessageServiceImpl]
   }
 }
